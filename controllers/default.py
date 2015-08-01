37a36e096cb179bb32c3a5d7512470c5ae4e033c
hkodungallur <hari.kodungallur@couchbase.com>
2015-08-01 18:37:30 +0530

    insert links to issues and reviews

--- controllers/default.py
+++ controllers/default.py
@@ -1,9 +1,12 @@
 import sys
 import os
+import re
 import datetime
 from subprocess import Popen, PIPE, STDOUT
 
-MYDIR = os.path.dirname(os.path.realpath(__file__))
+_MYDIR = os.path.dirname(os.path.realpath(__file__))
+_JIRA_PATTERN = r'(\bMB-\d+\b|\bCBD-\d+\b|\bCBSE-\d+\b)'
+_REW_PATTERN = r'(http://review.couchbase.org/\d+)'
 
 def get_log():
     sel_date = request.vars.q
@@ -11,11 +14,13 @@ def get_log():
         d = datetime.datetime.strptime(sel_date, "%Y-%m-%d")
     except ValueError:
         return "Error: Invalid date"
-    cmd = ['repo', 'forall', '-c', '%s/echo_log.sh' %MYDIR, sel_date]
+    cmd = ['repo', 'forall', '-c', '%s/echo_log.sh' %_MYDIR, sel_date]
     p = Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True, cwd='/home/couchbase/couchbase')
     output = p.stdout.read()
     if output:
-        return output
+        out1 = re.sub(_JIRA_PATTERN, r'<a href="https://issues.couchbase.com/browse/\1">\1</a>', output)
+        out2 = re.sub(_REW_PATTERN, r'<a href="\1">\1</a>', out1)
+        return out2
     else:
         return "No commits for %s" %sel_date
 
@@ -25,11 +30,13 @@ def get_diff():
         d = datetime.datetime.strptime(sel_date, "%Y-%m-%d")
     except ValueError:
         return "Error: Invalid date"
-    cmd = ['%s/day-builds.sh' %MYDIR, sel_date]
+    cmd = ['%s/day-builds.sh' %_MYDIR, sel_date]
     p = Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True, cwd='/home/couchbase/couchbase')
     output = p.stdout.read()
     if output:
-        return output
+        out1 = re.sub(_JIRA_PATTERN, r'<a href="https://issues.couchbase.com/browse/\1">\1</a>', output)
+        out2 = re.sub(_REW_PATTERN, r'<a href="\1">\1</a>', out1)
+        return out2
     else:
         return "No commits for %s" %sel_date
 
